a4e5c6126ccb1ac91a852a1e87aa19d261fb4ae7
Fiza Hussain <fiza16hussain@gmail.com>
2023-03-29 20:42:33 +0530

    added state to expenses form

--- src/Components/ExpenseForm.js
+++ src/Components/ExpenseForm.js
@@ -1,21 +1,37 @@
-import React from "react";
+import React , { useState } from "react";
 import "./ExpenseForm.css"
 
 export default function ExpenseForm() {
+
+    const [userTitle, setUserTitle] = useState("")
+    const [userAmount, setUserAmount] = useState("")
+    const [useDate, setUserDate] = useState("")
+
+    function updateUserTitle(event){
+        setUserTitle(event.target.value)
+    }
+
+    function updateUserAmount(event){
+        setUserAmount(event.target.value)
+    }
+
+    function updateUserDate(event){
+        setUserDate(event.target.value)
+    }
 
     return (
         <form className="expense-form" action="#">
             <div className="form-title">
                 <label htmlFor="#">Title</label><br />
-                <input type="text" />
+                <input type="text" onChange={updateUserTitle}/>
             </div>
             <div className="form-amount">
                 <label htmlFor="#">Amount</label><br />
-                <input type="number" />
+                <input type="number" onChange={updateUserAmount}/>
             </div>
             <div className="form-date">
                 <label htmlFor="#">Date</label><br />
-                <input type="date" />
+                <input type="date" onChange={updateUserDate}/>
             </div>
             <div>
                 <button className="form-add-btn">
